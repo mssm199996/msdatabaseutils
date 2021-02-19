2c8ccbcc573625b839c7a135353ddf417716ef7a
sidi mohamed moulay-slimane <mssm199996@gmail.com>
2021-02-19 17:37:42 +0100

    Added ICompte

---
+++ src/main/java/DomainModel/ICompte.java
@@ -0,0 +1,20 @@
+package DomainModel;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.List;
+
+public interface ICompte {
+
+	public abstract String getUsername();
+
+	public abstract String getPassword();
+
+	public abstract void setDateDerniereConnexion(LocalDate date);
+
+	public abstract void setHeureDerniereConnexion(LocalTime heure);
+
+	public abstract Boolean isAdmin();
+
+	public abstract List<Droit> getDroits();
+}
